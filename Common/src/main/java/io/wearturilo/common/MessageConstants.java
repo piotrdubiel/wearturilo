7efff722cf5c65355d490b120683090deb2ea375
Piotrek Dubiel <piotr.dubiel@polidea.com>
2015-03-06 17:07:15 +0100

    init

---
+++ Common/src/main/java/io/wearturilo/common/MessageConstants.java
@@ -0,0 +1,6 @@
+package io.wearturilo.common;
+
+public class MessageConstants {
+    public static final String RESULT_SUCCESS = "/result/success";
+    public static final String RESULT_FAILURE = "/result/failure";
+}
